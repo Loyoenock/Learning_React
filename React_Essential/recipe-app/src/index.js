55c3528823e12fe861c1aa2f73695556eff8f45f
Loyoenock <loyoenock@gmail.com>
2024-05-17 15:22:28 +0300

    React components

--- React_Essential/recipe-app/src/index.js
+++
@@ -1,7 +0,0 @@
-import React from "react"
-import { render } from "react-dom"
-import Menu from "./components/Menu"
-import data from "./data/recipe.json"
-
-render (<Menu recipes={data} />,
-document.getElementById("root"))
