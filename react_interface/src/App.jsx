5ff127940652aa9532fa03452c4c745276333353
Loyoenock <loyoenock@gmail.com>
2024-06-02 09:46:57 +0300

    Functionality - Delete appoitment implemented

--- react_interface/src/App.jsx
+++ react_interface/src/App.jsx
@@ -1,11 +1,24 @@
+import { useState, useEffect, useCallback } from "react";
 import { BiCalendar } from "react-icons/bi";
 import Search from "./components/Search.jsx"
 import AddAppointment from "./components/AddAppoitment.jsx";
 import AppointmentInfo from "./components/AppointmentInfo.jsx";
-import appointmentList from './data.json';
+
 
 function App() {
+  let [appointmentList, setAppointmentList] = useState([]);
 
+  const fetchData = useCallback(() => {
+    fetch('./data.json')
+      .then(response => response.json())
+      .then(data => {
+        setAppointmentList(data)      
+      });
+  }, [])
+
+  useEffect(() => {
+    fetchData()
+  }, [fetchData])
 
   return (
     <div className="App container mx-auto mt-3 font-thin">
@@ -18,7 +31,16 @@ function App() {
       <ul className="divide-y divide-gray-200">
         {
           appointmentList.map(appointment => (
-            <AppointmentInfo key={appointment.id} appointment={appointment} />
+            <AppointmentInfo key={appointment.id} 
+            appointment={appointment} 
+            onDeleteAppoitment={
+              appointmentId => {
+                setAppointmentList(appointmentList.filter(appointment =>
+                 appointment.id !== appointmentId))
+              
+              }
+            }
+             />
           ))
         }
       </ul>
