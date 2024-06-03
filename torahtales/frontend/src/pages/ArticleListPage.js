6179022ca7d27d4b343f7bd34d8d04fba47d9c5d
Loyoenock <loyoenock@gmail.com>
2024-06-03 16:04:30 +0300

    Component - Navbar

---
+++ torahtales/frontend/src/pages/ArticleListPage.js
@@ -0,0 +1,11 @@
+const ArticleListPage = () => {
+    return (
+        <div>
+            <h1>Articles</h1>
+            <p>
+                [List of articles will go here]
+            </p>
+        </div>
+    )
+}
+export default ArticleListPage;
---
+++ torahtales/frontend/src/pages/HomePage.js
@@ -0,0 +1,11 @@
+const HomePage = () => {
+    return (
+        <div>
+            <h1>Torah Tales</h1>
+            <p>
+                Welcome to Torah Tales! This is a place where you can read and share stories from the Torah.  
+            </p>
+        </div>
+    )
+}
+export default HomePage;
