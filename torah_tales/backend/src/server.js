b9601bedbb3e984a8eddfe5a7abca53c052e12b3
Loyoenock <loyoenock@gmail.com>
2024-06-06 15:35:04 +0300

    Comments endpoint added

--- torah_tales/backend/src/server.js
+++ torah_tales/backend/src/server.js
@@ -3,12 +3,15 @@ import express from 'express';
 let articlesInfo = [{
     name: 'parashat-intro',
     upvotes: 0,
+    comments: [],
 }, {
     name: 'stories-of-the-patriarchs',
     upvotes: 0,
+    comments: [],
 }, {
     name: 'wisdom-of-the-prophets',
     upvotes: 0,
+    comments: [],
 }]
 
 const app = express();
@@ -24,7 +27,21 @@ app.put('/api/articles/:name/upvote', (req, res) => {
     } else {
         res.send('That article doesn\'t exist!');
     }
-})
+});
+
+app.post('/api/articles/:name/comments', (req, res) => {
+    const { name } = req.params;
+    const { postedBy, text } = req.body;
+
+    const article = articlesInfo.find(a => a.name === name);
+
+    if (article){
+        article.comments.push({postedBy, text});
+        res.send(article.comments);
+    } else {
+        res.send('That article doesn\'t exist!');
+    }    
+}) 
 
 app.listen(8000, () => {
     console.log('Server is running on http://localhost:8000');
