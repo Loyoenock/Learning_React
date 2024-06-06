51ad02fd28b2abc827109546314b5ea0cb385056
Loyoenock <loyoenock@gmail.com>
2024-06-06 14:52:21 +0300

    Article Upvote

--- torah_tales/backend/src/server.js
+++ torah_tales/backend/src/server.js
@@ -1,9 +1,29 @@
 import express from 'express';
 
-const app = express();
+let articlesInfo = [{
+    name: 'parashat-intro',
+    upvotes: 0,
+}, {
+    name: 'stories-of-the-patriarchs',
+    upvotes: 0,
+}, {
+    name: 'wisdom-of-the-prophets',
+    upvotes: 0,
+}]
 
-app.get('/hello', (req, res) => {
-    res.send('Hello world!');
+const app = express();
+app.use(express.json());
+
+app.put('/api/articles/:name/upvote', (req, res) => {
+    const { name } = req.params;
+    const article = articlesInfo.find(a => a.name === name);
+
+    if (article) {
+        article.upvotes +=1;
+        res.send(`The ${name} article now has ${article.upvotes} upvotes`);
+    } else {
+        res.send('That article doesn\'t exist!');
+    }
 })
 
 app.listen(8000, () => {
